eb5f29fe001b5fc120a7ddd3ce341046db6b8f5c
Virvon <80401936+Virvon@users.noreply.github.com>
2022-12-10 20:58:30 +0300

    house alarm

---
+++ Scripts/PositionSetter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSetter : MonoBehaviour
+{
+    [SerializeField] private GameObject _character;
+
+    private Vector2 _position;
+    private Renderer _characterRenderer;
+
+    private void Awake()
+    {
+        _position = new Vector2(transform.position.x, transform.position.y);
+        _characterRenderer = _character.GetComponent<Renderer>();
+    }
+
+    public void Set(bool isCharacterInside)
+    {
+        _character.transform.position = _position;
+
+        if (isCharacterInside)
+            _characterRenderer.sortingOrder = 1;
+        else
+            _characterRenderer.sortingOrder = 5;
+    }
+}
